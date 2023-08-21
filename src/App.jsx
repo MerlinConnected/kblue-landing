3226e6ddc06db8465421f2feab62a285a6658d24
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-08-21 15:51:19 +0200

    feat: added key model & material

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Environment, Lightformer } from '@react-three/drei'
+import Key from './Key'
 
 export default function App() {
-  return (
-    <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color='darkblue' />
-      </mesh>
-      <OrbitControls />
-    </Canvas>
-  );
+	return (
+		<Canvas shadows orthographic camera={{ position: [10, 20, 20], zoom: 100 }}>
+			<Key rotation-x={Math.PI / 2} rotation-y={Math.PI} />
+			<OrbitControls />
+
+			{/* Environment & Lights */}
+			<Environment resolution={32}>
+				<group rotation={[-Math.PI / 4, -0.3, 0]}>
+					<Lightformer intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+					<Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
+					<Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
+					<Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 2, 1]} />
+					<Lightformer type='ring' intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
+				</group>
+			</Environment>
+		</Canvas>
+	)
 }
---
+++ src/Key.jsx
@@ -0,0 +1,65 @@
+import React, { useRef } from 'react'
+import { useGLTF, MeshTransmissionMaterial } from '@react-three/drei'
+import { RGBELoader } from 'three-stdlib'
+import { useLoader } from '@react-three/fiber'
+import { useControls } from 'leva'
+
+export default function Key(props) {
+	const { nodes, materials } = useGLTF('/Key.glb')
+	const texture = useLoader(
+		RGBELoader,
+		'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr'
+	)
+
+	const { ...config } = useControls({
+		backside: true,
+		backsideThickness: { value: 0.44, min: 0, max: 2 },
+		samples: { value: 128, min: 1, max: 1024, step: 1 },
+		resolution: { value: 1024, min: 64, max: 2048, step: 64 },
+		transmission: { value: 1, min: 0, max: 1 },
+		clearcoat: { value: 1, min: 0.1, max: 1 },
+		clearcoatRoughness: { value: 0.0, min: 0, max: 1 },
+		thickness: { value: 0.35, min: 0, max: 5 },
+		chromaticAberration: { value: 0, min: 0, max: 5 },
+		anisotropy: { value: 0.3, min: 0, max: 1, step: 0.01 },
+		roughness: { value: 0.48, min: 0, max: 1, step: 0.01 },
+		distortion: { value: 4, min: 0, max: 4, step: 0.01 },
+		distortionScale: { value: 0.4, min: 0.01, max: 1, step: 0.01 },
+		temporalDistortion: { value: 0.03, min: 0, max: 1, step: 0.01 },
+		ior: { value: 1.76, min: 0, max: 2, step: 0.01 },
+		color: '#737dd8',
+		gColor: '#ffffff',
+		shadow: '#ffffff',
+	})
+
+	return (
+		<group {...props} dispose={null}>
+			<mesh castShadow receiveShadow geometry={nodes.Cylinder.geometry}>
+				{/* <MeshTransmissionMaterial
+					backside={true}
+					backsideThickness={0.3}
+					samples={16}
+					resolution={1024}
+					transmission={1}
+					clearcoat={0.1}
+					clearcoatRoughness={0.25}
+					thickness={0.3}
+					chromaticAberration={5}
+					anisotropy={0.3}
+					roughness={0}
+					distortion={0.5}
+					distortionScale={0.1}
+					temporalDistortion={0.1}
+					ior={1.5}
+					color={'#ff9cf5'}
+					gcolor={'#ff7eb3'}
+					shadow={'#750d57'}
+					background={texture}
+				/> */}
+				<MeshTransmissionMaterial {...config} background={texture} />
+			</mesh>
+		</group>
+	)
+}
+
+useGLTF.preload('/Key.glb')
