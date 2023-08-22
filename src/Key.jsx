178d8c6ad9648cdf305216309995c2612049e7fd
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-08-22 14:59:06 +0200

    Fix: Cleanup

--- src/Key.jsx
+++ src/Key.jsx
@@ -1,40 +1,12 @@
 import React, { useRef } from 'react'
 import { useGLTF, MeshTransmissionMaterial } from '@react-three/drei'
-import { RGBELoader } from 'three-stdlib'
-import { useLoader } from '@react-three/fiber'
-import { useControls } from 'leva'
 
 export default function Key(props) {
 	const { nodes, materials } = useGLTF('/Key.glb')
-	const texture = useLoader(
-		RGBELoader,
-		'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr'
-	)
-
-	const { ...config } = useControls({
-		backside: true,
-		backsideThickness: { value: 0.44, min: 0, max: 2 },
-		samples: { value: 128, min: 1, max: 1024, step: 1 },
-		resolution: { value: 1024, min: 64, max: 2048, step: 64 },
-		transmission: { value: 1, min: 0, max: 1 },
-		clearcoat: { value: 1, min: 0.1, max: 1 },
-		clearcoatRoughness: { value: 0.0, min: 0, max: 1 },
-		thickness: { value: 0.35, min: 0, max: 5 },
-		chromaticAberration: { value: 0, min: 0, max: 5 },
-		anisotropy: { value: 0.3, min: 0, max: 1, step: 0.01 },
-		roughness: { value: 0.48, min: 0, max: 1, step: 0.01 },
-		distortion: { value: 4, min: 0, max: 4, step: 0.01 },
-		distortionScale: { value: 0.4, min: 0.01, max: 1, step: 0.01 },
-		temporalDistortion: { value: 0.03, min: 0, max: 1, step: 0.01 },
-		ior: { value: 1.76, min: 0, max: 2, step: 0.01 },
-		color: '#737dd8',
-		gColor: '#ffffff',
-		shadow: '#ffffff',
-	})
 
 	return (
 		<group {...props} dispose={null}>
-			<mesh castShadow receiveShadow geometry={nodes.Cylinder.geometry}>
+			<mesh castShadow receiveShadow geometry={nodes.Rounded_Key.geometry}>
 				<MeshTransmissionMaterial
 					backside
 					backsideThickness={0.44}
@@ -53,7 +25,6 @@ export default function Key(props) {
 					ior={1.5}
 					color={'#ffffff'}
 				/>
-				{/* <MeshTransmissionMaterial {...config} /> */}
 			</mesh>
 		</group>
 	)
