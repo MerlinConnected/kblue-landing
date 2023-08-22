48ce6dc0c4a2969d1091e4e6211d44e526a454ff
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-08-22 09:23:49 +0200

    Feat: Scene now moves on mouse move, tweaked textures & materials

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment, Lightformer } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Environment, Lightformer, Float } from '@react-three/drei'
 import Key from './Key'
+import { easing } from 'maath'
+import Spheres from './Spheres'
 
 export default function App() {
 	return (
-		<Canvas shadows orthographic camera={{ position: [10, 20, 20], zoom: 100 }}>
-			<Key rotation-x={Math.PI / 2} rotation-y={Math.PI} />
-			<OrbitControls />
+		<Canvas shadows orthographic camera={{ position: [15, -5, 30], zoom: 100 }}>
+			<Float floatIntensity={0.1} speed={0.2} floatingRange={[1, 10]}>
+				<Key rotation-x={Math.PI / 2} rotation-y={Math.PI} />
+				<Spheres />
+			</Float>
+			{/* <OrbitControls /> */}
 
 			{/* Environment & Lights */}
 			<Environment resolution={32}>
@@ -18,6 +23,22 @@ export default function App() {
 					<Lightformer type='ring' intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
 				</group>
 			</Environment>
+
+			<ambientLight intensity={0.75} />
+
+			<Rig />
 		</Canvas>
 	)
+}
+
+function Rig() {
+	useFrame((state, delta) => {
+		easing.damp3(
+			state.camera.position,
+			[Math.sin(-state.pointer.x) * 5, state.pointer.y * 3.5, 15 + Math.cos(state.pointer.x) * 10],
+			0.2,
+			delta
+		)
+		state.camera.lookAt(0, 0, 0)
+	})
 }
